347c5596c4b3530594565c279e1d7a4071acfaef
Toub <n.toublanc@gmail.com>
2015-11-09 19:37:33 +0000

    Create empty test file

---
+++ client/src/app/profile/user-repository/user.generator.service.spec.js
@@ -0,0 +1,10 @@
+(function () {
+    'use strict';
+
+    // TODO s'inspirer de app/components/form/password/form.password.service.spec.js
+    // pour tester user.generator.service.js
+    // - test de generateUser qui vérifie que l'objet retourné à un email et une date de naissance valide
+    // - test de generateUsers qui vérifie que le nombre d'users retourné correspond à celui passé en paramètre
+    // - test de generateUsers avec des valeurs limites (0, -1)
+    
+})();
